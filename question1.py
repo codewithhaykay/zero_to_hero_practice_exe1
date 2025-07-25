5a83af5feb0027d7b6d5b257d079ee7ee54fb9d2
codewithhaykay <digitalmagnet506@gmail.com>
2025-07-25 21:21:43 +0100

    question 1 of the practice exercises solved.

---
+++ question1.py
@@ -0,0 +1,6 @@
+###This program calculate the area of a circle given its radius.
+
+PI = 3.142
+radius = float(input("Enter the radius of the circle: "))
+area = PI * radius ** 2
+print(f"The area of the circle with radius {radius} is: {area}")
